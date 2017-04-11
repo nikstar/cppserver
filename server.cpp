8f328e386c75826d8c201398ef495b738bd497df
nikstar <nstarshinov@gmail.com>
2017-04-11 00:04:10 -0700

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,41 +1,25 @@
 #include <iostream>
-#include "sock_wrap.h"
+#include "net.h"
 
-using namespace std;
-using namespace SockWrap;
 
-#define PORT_NUM 1234  // номер порта процесса-сервера 
-
-class MyServerSocket : public InServerSocket {
-public:
-    MyServerSocket () : InServerSocket (PORT_NUM) {}
-protected:
-    void OnAccept (BaseSocket * pConn)
-    {
-    // установлено соединение с клиентом, читаем сообщение
-    cout << "Read from client: " << pConn->GetString() << endl;
-    // отправляем ответ
-    pConn->PutString("Hello from server.");
-
-    // продолжаем диалог с клиентом, пока в этом есть необходимость
-    // ...
-
-    delete pConn;
+void handle(Conn conn) {
+    if (!fork()) {
+        auto str = conn.ReadString();
+        std::cout << "Recieved from client: " << str << std::endl;
+        conn.WriteString("Hello from server");
     }
-};
+}
 
 int main(int argc, char* argv[])
 {
     try {
-      // создаём серверный сокет
-      MyServerSocket sock;
-
-      for (;;)
-          // слушаем запросы на соединение
-          sock.Accept();
-    } catch (Exception& e) {
-      // ошибка --- выводим текст сообщения на экран
-      e.Report();
+        auto port = 8081;
+        auto l = net::Listen(port);
+        
+        for (;;) {
+            auto conn = l.Accept();
+            handle(conn);
+        }
     }	
     return 0;
 }
